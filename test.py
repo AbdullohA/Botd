edbb9f9ead3b84b88d91e98a2d427f9dba448b3e
AbdullohA <86557557+AbdullohA@users.noreply.github.com>
2022-08-28 12:43:27 +0500

    Update test.py

--- test.py
+++ test.py
@@ -10,14 +10,18 @@ LARGE_FILE = "50 MBdan katta bo'lgan faylga path"
 
 @dp.message_handler(content_types=['text'])
 async def large_file(message: types.Message):
-	yt = YouTube(message.text)
-	# yt = yt.streams.filter(progressive=True,   file_extension='mp4').order_by('resolution').desc().first()
-	yt = yt.streams.get_highest_resolution()
+	if message.text == '/start':
+		await bot.send_message(message.chat.id,'manga silka tasha')
+	else:
+		print(message.text)
+		yt = YouTube(message.text)
+		# yt = yt.streams.filter(progressive=True,   file_extension='mp4').order_by('resolution').desc().first()
+		yt = yt.streams.get_highest_resolution()
 
-	a=yt.download('videos')
-	videos = open(a, 'rb')
+		a=yt.download('videos')
+		videos = open(a, 'rb')
 
-	await bot.send_video(message.chat.id, videos)
+		await bot.send_video(message.chat.id, videos)
 
 
 if __name__ == '__main__':
