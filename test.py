54aeac6f4c3addc78c6873a11642f0e83f9559a0
AbdullohA <86557557+AbdullohA@users.noreply.github.com>
2022-08-28 11:41:34 +0500

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.bot.api import TelegramAPIServer
-
+import pytube
 
 local_server = TelegramAPIServer.from_base('http://localhost:8081')
 bot = Bot(token='5339728805:AAHoSmbplCiq93adu5Bo3IX1wgR-UphokJg', server=local_server)
@@ -10,14 +10,14 @@ LARGE_FILE = "50 MBdan katta bo'lgan faylga path"
 
 @dp.message_handler(commands=['largefile'])
 async def large_file(message: types.Message):
-	                yt = YouTube(link)
-                        # yt = yt.streams.filter(progressive=True,   file_extension='mp4').order_by('resolution').desc().first()
-                        yt = yt.streams.get_highest_resolution()
+	yt = YouTube(link)
+	# yt = yt.streams.filter(progressive=True,   file_extension='mp4').order_by('resolution').desc().first()
+	yt = yt.streams.get_highest_resolution()
 
-                        a=yt.download('videos')
-                        videos = open(a, 'rb')
+	a=yt.download('videos')
+	videos = open(a, 'rb')
 
-                        await bot.send_video(message.chat.id, videos)
+	await bot.send_video(message.chat.id, videos)
 
 
 if __name__ == '__main__':
